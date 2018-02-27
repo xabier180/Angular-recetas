27936c85f599490c5a80ae5bb6ec856d94d795ef
Curso <Curso@IPARTEKAULA.COM>
2018-02-27 13:32:31 +0100

    completar receta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RecetaComponent } from './receta/receta.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecetaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/receta/receta.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receta',
+  templateUrl: './receta.component.html',
+  styleUrls: ['./receta.component.scss']
+})
+export class RecetaComponent implements OnInit {
+
+  //atributos
+
+  nombre: string;
+  descripcion: string;
+  likes: number;
+  foto: string;
+  isGlutenFree: boolean;
+  cocinero: string;
+  ingredientes: string[];
+
+  constructor() {
+    console.log('RecetaComponent constructor');
+    this.nombre = 'Bokata de calamares';
+    this.descripcion = 'Bocadillo típico madrileño de calamares a la romana con mayonesa y un buen chorro de limón.'
+    this.likes = 334;
+    this.foto = 'https://www.hogarmania.com/archivos/201411/bocadillo-calamares-xl-668x400x80xX.jpg';
+    this.isGlutenFree = true; 
+    this.cocinero = "Karlos Arguiñano";
+    this.ingredientes = ['Calamares', 'Pan', 'Salsa Ali Oli', 'Limón'];
+  }
+
+  ngOnInit() {
+    console.log('RecetaComponent ngOnInit');
+  }
+
+  sumarLike(){
+    console.log('Click sumarLike');
+    this.likes++;
+  }
+
+}
